1e43ba2ab74c195cd01a7348fe48f0f7e57a5692
9ta9ta <cocoon_archer@yahoo.co.jp>
2025-10-30 22:29:12 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const resultContainer = document.getElementById('resultContainer');
 const resultImage = document.getElementById('resultImage');
 const resultText = document.getElementById('resultText');
 
+// 効果音
+const soundA = new Audio('kozutumi.mp3');
+const soundB = new Audio('pahu.mp3');
+
 const results = [
   { a: 'result_01_a.png', b: 'result_01_b.png' },
   { a: 'result_02_a.png', b: 'result_02_b.png' },
@@ -25,15 +29,23 @@ fortuneBtn.addEventListener('click', () => {
 
   resultContainer.classList.remove('hidden');
   resultImage.src = currentResult.a;
+  resultImage.classList.remove('b-small'); // A画像は通常サイズ
   resultText.textContent = 'ミーをクリックするざんす！';
   showingB = false;
+
+  soundA.currentTime = 0; // 再生位置リセット
+  soundA.play();           // 効果音再生
 });
 
 resultImage.addEventListener('click', () => {
   if (!showingB) {
     resultImage.src = currentResult.b;
+    resultImage.classList.add('b-small'); // B画像を小さく
     resultText.textContent = 'あとは自力で叶えるざんす〜';
     showingB = true;
+
+    soundB.currentTime = 0;
+    soundB.play(); // B画像効果音再生
   } else {
     resultContainer.classList.add('hidden');
     topImage.classList.remove('hidden');
